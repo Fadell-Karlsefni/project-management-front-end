8c51b417d7385f36edcf621a50bd6fff2798cb15
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-09-21 20:56:05 +0800

    Setup dan modifikasi untuk Eslint dan menambahkan prettierignore serta mencoba cek npm lint, fix nya serta npm format nya

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { ThemeProvider, CssBaseline, createTheme, Button } from '@mui/material';
+import { Button, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 const theme = createTheme({
   typography: {
-    fontFamily: ["Roboto", "sans-serif"].join(","),
+    fontFamily: ['Roboto', 'sans-serif'].join(','),
   },
 });
 
 const Home = () => {
-  return (
-    <Button variant="contained">Hallo, ini tombol MUI</Button>
-  );
+  return <Button variant="contained">Hallo, ini tombol MUI</Button>;
 };
 
 const App = () => {
