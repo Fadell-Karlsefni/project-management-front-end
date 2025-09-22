ea3ae70a389faf150bb970a176b5c8a2eab6eb94
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-09-22 11:39:14 +0800

    setup dan install package react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { Button, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import {
+  Box,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
 
 const theme = createTheme({
   typography: {
@@ -6,15 +13,32 @@ const theme = createTheme({
   },
 });
 
-const Home = () => {
-  return <Button variant="contained">Hallo, ini tombol MUI</Button>;
-};
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Box>
+        <Typography variant="h1">Home</Typography>
+        <Link to="/login">Login</Link>
+      </Box>
+    ),
+  },
+  {
+    path: '/login',
+    element: (
+      <Box>
+        <Typography variant="h1">Login</Typography>
+        <Link to="/">Kembali ke Home</Link>
+      </Box>
+    ),
+  },
+]);
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Home />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 };
