c4ad31974546ea5c78640de5a5b296447a58c83d
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-09-23 09:25:51 +0800

    Mencoba Membuat komponen table

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import {
 } from '@mui/material';
 import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
 
+import Table from './components/ui/table';
+
 const theme = createTheme({
   typography: {
     fontFamily: ['Roboto', 'sans-serif'].join(','),
@@ -18,8 +20,35 @@ const router = createBrowserRouter([
     path: '/',
     element: (
       <Box>
-        <Typography variant="h1">Home</Typography>
-        <Link to="/login">Login</Link>
+        <Table
+          columns={[
+            {
+              id: 'tugas',
+              label: 'tugas',
+            },
+            {
+              id: 'status',
+              label: 'status',
+            },
+          ]}
+          data={[
+            {
+              id: 1,
+              tugas: 'tugas 1',
+              status: 'baru',
+            },
+            {
+              id: 2,
+              tugas: 'tugas 2',
+              status: 'sedang di kerjakan',
+            },
+            {
+              id: 3,
+              tugas: 'tugas 3',
+              status: 'selesai',
+            },
+          ]}
+        />
       </Box>
     ),
   },
